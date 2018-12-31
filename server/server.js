83ea2d12542a6fd98fc0ec3c49bff4a4f94c8c85
Kirti Jha <kirtijha@in.ibm.com>
2018-12-31 15:17:51 +0530

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 let _ = require('lodash')
 let express = require('express');
 let bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const {ObjectID} = require('mongodb');
 
@@ -112,6 +113,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {    // PRIVATE ROUTE WITH MIDDLEWARE
  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+    });
+    })
+    .catch((e) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
